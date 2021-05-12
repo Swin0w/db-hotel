09efdf9e353864625606fe86ef8f454917927f8d
Simone <marzolla.istruzione@gmail.com>
2021-05-12 09:38:24 +0200

    Query corrette

--- esericizio.sql
+++ esericizio.sql
@@ -1,7 +1,7 @@
 SELECT * FROM ospiti;
-SELECT * FROM ospiti WHERE document_type = 'CI';
-SELECT * FROM paganti WHERE ospite_id NOT NULL;
+SELECT * FROM ospiti WHERE document_type LIKE 'CI';
+SELECT * FROM paganti WHERE ospite_id IS NOT NULL;
 SELECT * FROM stanze WHERE floor = 1;
 SELECT 'E' FROM ospiti; --Non sono sicuro
 SELECT * FROM ospiti WHERE date_of_birth = DATE(DATE_SUB(NOW(), INTERVAL 30 YEAR));
-SELECT * FROM prenotazioni WHERE created_at < '2018-05-28'; 
+SELECT * FROM prenotazioni WHERE created_at < '2018-05-28';
