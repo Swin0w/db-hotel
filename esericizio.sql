5fa550fb4d03992063c295873e1c57d14387c864
Simone <marzolla.istruzione@gmail.com>
2021-05-13 12:32:40 +0200

    Esercizio 2 Foglio 1 Fatto

--- esericizio.sql
+++ esericizio.sql
@@ -9,5 +9,39 @@ SELECT * FROM prenotazioni WHERE created_at < '2018-05-28';
 -- Query giuste
 SELECT * FROM ospiti WHERE document_type LIKE 'CI';
 SELECT * FROM paganti WHERE ospiti_id IS NOT NULL
-SELECT * FORM ospiti WHERE name LIKE 'E%' && 'e%'
+SELECT * FROM ospiti WHERE name LIKE 'E%' && 'e%'
 SELECT * FROM ospiti WHERE YEAR(NOW()) - YEAR(date_of_birth) < 30
+
+
+
+-- Continuo db-hotel -> FOGLIO 1
+SELECT ospiti.id, name, lastname, prenotazioni_has_ospiti.prenotazione_id, COUNT(prenotazione_id)
+FROM ospiti
+JOIN prenotazioni_has_ospiti ON ospiti.id=prenotazioni_has_ospiti.ospite_id
+GROUP BY ospiti.id
+HAVING COUNT(prenotazione_id) > 2
+-- 1
+SELECT ospiti.id,name,lastname, prenotazioni.stanza_id,prenotazioni.configurazione_id
+FROM ospiti
+JOIN prenotazioni_has_ospiti
+ON prenotazioni_has_ospiti.ospite_id = ospiti.id
+JOIN prenotazioni
+ON prenotazioni_has_ospiti.prenotazione_id=prenotazioni.id
+ORDER BY prenotazioni.configurazione_id DESC
+-- 2
+
+-- 3
+
+
+
+
+-- Continuo db-hotel -> FOGLIO 2
+SELECT * FROM ospiti ORDER BY date_of_birth ASC -- Sbagliata | 1
+SELECT *, SUM(price) FROM pagamenti GROUP BY status -- 2
+SELECT stanza_id, COUNT(stanza_id) FROM prenotazioni GROUP BY stanza_id -- 3
+SELECT COUNT(HOUR(created_at)), HOUR(created_at) FROM prenotazioni GROUP BY HOUR(created_at) ORDER BY COUNT(HOUR(created_at)) -- 4
+SELECT COUNT(prenotazione_id), ospite_id FROM prenotazioni_has_ospiti GROUP BY ospite_id ORDER BY COUNT(prenotazione_id) DESC -- 5
+
+
+-- Query Giuste
+SELECT COUNT(*), YEAR(date_of_birth) FROM ospiti GROUP BY YEAR(date_of_birth) -- 1
